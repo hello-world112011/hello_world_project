315092b7345ccc169c94008f039dcb85cdc89375
hello-world112011 <tie_gullet0d@icloud.com>
2024-06-02 22:45:20 +0300

    test

---
+++ src/test/java/com/docker_hello_world/AppTest.java
@@ -0,0 +1,12 @@
+package com.docker_hello_world;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    public void yee()
+    {
+        System.out.println("works!");
+    }
+}
